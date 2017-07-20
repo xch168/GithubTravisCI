5da0d29f921fd7b9e0afbb2d28a74f9422e039fd
徐灿辉 <xucanhui@4399inc.com>
2017-07-20 19:06:28 +0800

    init

---
+++ app/src/main/java/com/m4399/githubtravisci/MainActivity.java
@@ -0,0 +1,15 @@
+package com.m4399.githubtravisci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
